0b96a7ae91b999935dfa15877e299a8224f9902e
Keita <24019755@student.hhs.nl>
2025-04-15 14:23:34 +0200

    bericht Koppelingen
    deze bericht koppeling zorgt ervoor dat een gebruiker berichten kan koppelen aan userstories, epics en taken

--- TeamFlow groep F/src/main/java/Domein/Bericht.java
+++ TeamFlow groep F/src/main/java/Domein/Bericht.java
@@ -8,14 +8,28 @@ public class Bericht {
     private Timestamp tijdStamp;
     private String bericht;
     private int idGebruiker;
+    private ScrumItem gekoppeldItem;
 
-    public Bericht(int idBericht, Timestamp tijdStamp, String bericht, int idGebruiker) {
+    public Bericht(int idBericht, Timestamp tijdStamp, String bericht, int idGebruiker, ScrumItem gekoppeldItem) {
         this.idBericht = idBericht;
         this.tijdStamp = tijdStamp;
         this.bericht = bericht;
         this.idGebruiker = idGebruiker;
+        this.gekoppeldItem = gekoppeldItem;
+    }
+    public Bericht(int idBericht, Timestamp tijdStamp, String bericht, int idGebruiker) {
+        this(idBericht, tijdStamp, bericht, idGebruiker, null);
     }
 
+    public ScrumItem getGekoppeldItem() {
+        return gekoppeldItem;
+    }
+    public void setGekoppeldItem(ScrumItem gekoppeldItem) {
+        this.gekoppeldItem = gekoppeldItem;
+    }
 
-
+    @Override
+    public String toString() {
+        return "idGebruiker: [" + idGebruiker + "] " + (gekoppeldItem == null ? "Geen koppeling" : gekoppeldItem.getNaam()) + " \nidBericht: [" + idBericht + "]" + " bericht: " + bericht + " \ntijdStamp: " + tijdStamp + ""
+    }
 }
