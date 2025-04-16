0fc2f1f971a1d8c8e8fc9daa5240fc6e073aef44
Keita <24019755@student.hhs.nl>
2025-04-16 21:48:11 +0200

    Bericht binnen taken en userstories kan gemaakt worden

--- TeamFlow groep F/src/main/java/Domein/Taken.java
+++ TeamFlow groep F/src/main/java/Domein/Taken.java
@@ -1,14 +1,20 @@
 package Domein;
 
 import Utils.DatabaseUtil;
+import Utils.Session;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Taken extends ScrumItem{
     private int UserStory_idUserStory;
     private int idTaken;
+    ArrayList<GebruikerHasScrumItem> gebruikers = new ArrayList<>();
 
     public Taken (int UserStory_idUserStory, int IdTaken, String scrumItemNaam) {
         super(scrumItemNaam);
@@ -30,6 +36,33 @@ public class Taken extends ScrumItem{
             statement.setInt(2, this.idTaken);
             statement.executeUpdate();
         }
+    }
+    public void BerichtAanmaken (Scanner scanner) throws SQLException {
+        System.out.println("Typ hieronder het bericht dat je wilt posten (enter om te versturen): ");
+        String berichtTekst = scanner.nextLine();
+
+        if (berichtTekst.isBlank()) {
+            System.out.println("Bericht mag niet leeg zijn!");
+            return;
+        }
+
+        LocalDate currentDate = LocalDate.now();
+        Date sqlDate = Date.valueOf(currentDate);
+
+        try (Connection connection = DatabaseUtil.getConnection()) {
+            String query = "INSERT INTO Bericht_taken " +
+                    "(tijdStamp, bericht, gebruiker_idGebruiker, taken_idTaken)" +
+                    " VALUES (?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setDate(1, sqlDate);
+            statement.setString(2, berichtTekst);
+            statement.setInt(3, Session.getActiveGebruiker().getIdGebruiker());
+            statement.setInt(4, idTaken);
+            statement.executeUpdate();
+        }
+
+        Bericht bericht = new Bericht(-1, sqlDate, berichtTekst, Session.getActiveGebruiker().getIdGebruiker(), null);
+        System.out.println("Bericht aangemaakt voor de taak!");
     }
 
 
--- TeamFlow groep F/src/main/java/Domein/UserStory.java
+++ TeamFlow groep F/src/main/java/Domein/UserStory.java
@@ -1,16 +1,21 @@
 package Domein;
 
 import Utils.DatabaseUtil;
+import Utils.Session;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class UserStory extends ScrumItem  implements IZoek {
     private int idUserStory;
     private int Epic_idEpic;
     private ArrayList<Taken> taken;
+    private ArrayList<GebruikerHasScrumItem> gebruikers = new ArrayList<>();
 
     public UserStory(int idUserStory, int Epic_IdEpic, String scrumItemNaam) {
         super(scrumItemNaam);
@@ -42,5 +47,32 @@ public class UserStory extends ScrumItem  implements IZoek {
             statement.setInt(2, this.idUserStory);
             statement.executeUpdate();
         }
+    }
+    public void BerichtAanmaken (Scanner scanner) throws SQLException {
+        System.out.println("Typ hieronder het bericht dat je wilt posten (enter om te versturen): ");
+        String berichtTekst = scanner.nextLine();
+
+        if (berichtTekst.isBlank()) {
+            System.out.println("Bericht mag niet leeg zijn!");
+            return;
+        }
+
+        LocalDate currentDate = LocalDate.now();
+        Date sqlDate = Date.valueOf(currentDate);
+
+        try (Connection connection = DatabaseUtil.getConnection()) {
+            String query = "INSERT INTO Bericht_UserStory " +
+                    "(tijdStamp, bericht, gebruiker_idGebruiker, user_story_idUserStory)" +
+                    " VALUES (?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setDate(1, sqlDate);
+            statement.setString(2, berichtTekst);
+            statement.setInt(3, Session.getActiveGebruiker().getIdGebruiker());
+            statement.setInt(4, idUserStory);
+            statement.executeUpdate();
+        }
+
+        Bericht bericht = new Bericht(-1, sqlDate, berichtTekst, Session.getActiveGebruiker().getIdGebruiker(), null);
+        System.out.println("Bericht aangemaakt voor de user story!");
     }
 }
