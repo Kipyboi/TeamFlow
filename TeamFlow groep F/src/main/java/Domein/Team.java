1d9d21803b834731cf4eb9f1e03f7473a7fe0b1c
Keita <24019755@student.hhs.nl>
2025-04-17 01:59:47 +0200

    team kan aangemaakt worden

--- TeamFlow groep F/src/main/java/Domein/Team.java
+++ TeamFlow groep F/src/main/java/Domein/Team.java
@@ -5,6 +5,7 @@ import Utils.DatabaseUtil;
 import javax.xml.crypto.Data;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -14,17 +15,19 @@ public class Team implements IZoek {
     private ArrayList<GebruikerHasTeam> gebruikers;
     private ArrayList<ScrumItem> scrumItems;
 
-    public Team (int idTeam, String TeamNaam) {
+    public Team(int idTeam, String TeamNaam) throws SQLException {
         this.idTeam = idTeam;
         this.teamNaam = TeamNaam;
         gebruikers = new ArrayList<>();
         scrumItems = new ArrayList<>();
+        voegTeamToe(TeamNaam);
     }
+
     public void gebruikerToevoegen(Gebruiker gebruiker) throws SQLException {
         GebruikerHasTeam ght = new GebruikerHasTeam(gebruiker, this);
         gebruiker.addTeam(ght);
         gebruikers.add(ght);
-        try (Connection connection = DatabaseUtil.getConnection()){
+        try (Connection connection = DatabaseUtil.getConnection()) {
             String query = "INSERT INTO gebruiker_has_team (gebruiker_idGebruiker, team_idteam) VALUES (?,?)";
             PreparedStatement statement = connection.prepareStatement(query);
             statement.setInt(1, gebruiker.getIdGebruiker());
@@ -47,13 +50,11 @@ public class Team implements IZoek {
                 ght.getGebruiker().removeTeam(ght);
                 gebruikers.remove(ght);
             }
-
         }
     }
 
 
-
-    public ScrumItem zoek (String zoekterm) {
+    public ScrumItem zoek(String zoekterm) {
         for (ScrumItem scrumItem : scrumItems) {
             if (scrumItem.getScrumItemNaam().toLowerCase().contains(zoekterm.toLowerCase())) {
                 return scrumItem;
@@ -68,5 +69,22 @@ public class Team implements IZoek {
 
     public void setGebruikers(ArrayList<GebruikerHasTeam> gebruikers) {
         this.gebruikers = gebruikers;
+    }
+
+    public void voegTeamToe(String teamNaam) throws SQLException {
+        try (Connection connection = DatabaseUtil.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO team (teamNaam) VALUES (?)", PreparedStatement.RETURN_GENERATED_KEYS);
+
+            statement.setString(1, teamNaam);
+            statement.executeUpdate();
+
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    this.idTeam = generatedKeys.getInt(1);
+                    System.out.println("Team: " + teamNaam + " is toegevoegd.");
+                }
+            }
+        }
     }
 }
