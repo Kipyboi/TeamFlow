7aa345b45649d82042cc0b00cb4219d4dced1b7f
BjornS2006 <24074918@student.hhs.nl>
2025-04-19 11:36:57 +0200

    Berichten posten
    je kan nu berichten posten

--- TeamFlow groep F/src/main/java/Domein/IMenu.java
+++ TeamFlow groep F/src/main/java/Domein/IMenu.java
@@ -7,22 +7,7 @@ public interface IMenu {
     default void menu(Scanner scanner) throws SQLException {
         System.out.println("you're not allowed to do this");
     }
-    default void GebruikerAanmaken() {
-        System.out.println("you're not allowed to do this");
-    }
-    default void GebruikerInloggen() {
-        System.out.println("you're not allowed to do this");
-    }
     default void BerichtAanmaken(Scanner scanner) throws SQLException {
-        System.out.println("you're not allowed to do this");
-    }
-    default void GaNaar() {
-        System.out.println("you're not allowed to do this");
-    }
-    default void GaTerug() {
-        System.out.println("you're not allowed to do this");
-    }
-    default void TeamAanmaken(Scanner scanner)throws SQLException {
         System.out.println("you're not allowed to do this");
     }
     default void EpicAanmaken(Scanner scanner)throws SQLException {
