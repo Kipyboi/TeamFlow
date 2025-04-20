3052b200e0ddba5a46cecdd8547b8bb6643050cf
BjornS2006 <24074918@student.hhs.nl>
2025-04-20 23:44:55 +0200

    bug fix

--- TeamFlow groep F/src/main/java/Tests/GebruikerHasScrumItemTest.java
+++ TeamFlow groep F/src/main/java/Tests/GebruikerHasScrumItemTest.java
@@ -6,10 +6,13 @@ import Domein.ScrumItem;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 class TestScrumItem extends ScrumItem {
-    public TestScrumItem(String scrumItemNaam, String beschrijving) {
+    public TestScrumItem(String scrumItemNaam, String beschrijving) throws SQLException {
+        super(scrumItemNaam, beschrijving);
         this.scrumItemNaam = scrumItemNaam;
         this.beschrijving = beschrijving;
     }
@@ -26,7 +29,7 @@ class TestScrumItem extends ScrumItem {
 public class GebruikerHasScrumItemTest {
 
     @Test
-    public void testGebruikerHasScrumItemCreation() {
+    public void testGebruikerHasScrumItemCreation() throws SQLException {
         // Arrange: Create test objects
         Gebruiker gebruiker = new Gebruiker(1, "TestUser");
         ScrumItem scrumItem = new TestScrumItem("Login Feature", "Enhance security");
